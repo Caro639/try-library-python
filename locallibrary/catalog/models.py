80b2f0cd311b08d6b3db1370ce7d308ef8187dcb
Caro639 <120673222+Caro639@users.noreply.github.com>
2026-03-02 18:04:24 +0100

    add language + admin

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse  # Cette fonction est utilisée pour formater les URL
 import uuid  # Ce module est nécessaire à la gestion des identifiants unique (RFC 4122) pour les copies des livres
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
 
 
 # Create your models here.
@@ -8,7 +10,9 @@ class Genre(models.Model):
     """Cet objet représente une catégorie ou un genre littéraire."""
 
     name = models.CharField(
-        max_length=200, help_text="Enter a book genre (e.g. Science Fiction)"
+        max_length=200,
+        unique=True,
+        help_text="Enter a book genre (e.g. Science Fiction)",
     )
 
     def __str__(self):
@@ -16,6 +20,48 @@ class Genre(models.Model):
         Elle retourne une chaîne de caractère pour identifier l'instance de la classe d'objet.
         """
         return self.name
+
+    def get_absolute_url(self):
+        """Cette fonction est requise pas Django, lorsque vous souhaitez détailler le contenu d'un objet."""
+        return reverse("genre-detail", args=[str(self.id)])
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower("name"),
+                name="genre_name_case_insensitive_unique",
+                violation_error_message="Genre with this name already exists (case-insensitive).",
+            ),
+        ]
+
+
+class Language(models.Model):
+    """Cet objet représente une langue (e.g. Anglais, Français, Japonais, etc.)."""
+
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Enter a book language (e.g. English, French, Japanese)",
+    )
+
+    def __str__(self):
+        """Cette fonction est obligatoirement requise par Django.
+        Elle retourne une chaîne de caractère pour identifier l'instance de la classe d'objet.
+        """
+        return self.name
+
+    def get_absolute_url(self):
+        """Cette fonction est requise pas Django, lorsque vous souhaitez détailler le contenu d'un objet."""
+        return reverse("language-detail", args=[str(self.id)])
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower("name"),
+                name="language_name_case_insensitive_unique",
+                violation_error_message="Language with this name already exists (case-insensitive).",
+            ),
+        ]
 
 
 class Book(models.Model):
@@ -27,7 +73,7 @@ class Book(models.Model):
     #  Un livre a un seul auteur, mais un auteur a écrit plusieurs livres.
     # Le type de l'objet Author est déclré comme une chaîne de caractère car
     # la classe d'objet Author n'a pas encore été déclarée dans le fichier
-    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey("Author", on_delete=models.RESTRICT, null=True)
 
     summary = models.TextField(
         max_length=1000, help_text="Enter a brief description of the book"
@@ -43,6 +89,17 @@ class Book(models.Model):
     # Comme la classe d'objets Genre a été définit précédemment, nous pouvons manipuler l'objet.
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
+    language = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ["title", "author"]
+
+    def display_genre(self):
+        """Cette fonction est utilisée pour afficher le genre dans l'admin de Django."""
+        return ", ".join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = "Genre"
+
     def __str__(self):
         """Fonction requise par Django pour manipuler les objets Book dans la base de données."""
         return self.title
@@ -50,6 +107,11 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Cette fonction est requise pas Django, lorsque vous souhaitez détailler le contenu d'un objet."""
         return reverse("book-detail", args=[str(self.id)])
+
+
+from datetime import date
+
+from django.conf import settings  # Required to assign User as a borrower
 
 
 class BookInstance(models.Model):
@@ -60,9 +122,17 @@ class BookInstance(models.Model):
         default=uuid.uuid4,
         help_text="Unique ID for this particular book across whole library",
     )
-    book = models.ForeignKey("Book", on_delete=models.SET_NULL, null=True)
+    book = models.ForeignKey("Book", on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True
+    )
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
     LOAN_STATUS = (
         ("m", "Maintenance"),
@@ -81,6 +151,10 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
+
+    def get_absolute_url(self):
+        """Cette fonction est requise pas Django, lorsque vous souhaitez détailler le contenu d'un objet."""
+        return reverse("bookinstance-detail", args=[str(self.id)])
 
     def __str__(self):
         """Fonction requise par Django pour manipuler les objets BookInstance dans la base de données."""
