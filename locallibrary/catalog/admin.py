0d6d28c1d65751bf5b72df77d1952b8cabdafce2
Caro639 <120673222+Caro639@users.noreply.github.com>
2026-03-04 13:56:44 +0100

    add dynamic homepage

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,16 +4,22 @@ from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance, Language
 
 
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+
+    model = Book
+
+
 # admin.site.register(Book)
 # admin.site.register(Author)
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
     fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
-    pass
+    inlines = [BooksInline]
 
 
-admin.site.register(Author, AuthorAdmin)
+admin.site.register(Author)
 
 
 class BooksInstanceInline(admin.TabularInline):
@@ -21,23 +27,25 @@ class BooksInstanceInline(admin.TabularInline):
 
 
 # Register the Admin classes for Book using the decorator
-@admin.register(Book)
+# @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "display_genre")
     inlines = [BooksInstanceInline]
-    pass
+
+
+admin.site.register(Book, BookAdmin)
 
 
 # Register the Admin classes for BookInstance using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ("book", "status", "borrower", "due_back", "id")
     list_filter = ("status", "due_back")
 
     fieldsets = (
         (None, {"fields": ("book", "imprint", "id")}),
-        ("Availability", {"fields": ("status", "due_back")}),
+        ("Availability", {"fields": ("status", "due_back", "borrower")}),
     )
-    pass
 
 
 admin.site.register(Genre)
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -14,23 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+
 # Use include() to add paths from the catalog application
 from django.urls import include
+from catalog import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 urlpatterns += [
-    path('catalog/', include('catalog.urls')),
+    path("catalog/", include("catalog.urls")),
 ]
 
-#Add URL maps to redirect the base URL to our application
+
+# Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
+
 urlpatterns += [
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path("", RedirectView.as_view(url="/catalog/", permanent=True)),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
